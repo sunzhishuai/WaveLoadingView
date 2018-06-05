4a3005fcea1acdfdc172fc4942020fbe2caae621
sunzhishuai <553806014@qq.com>
2018-06-05 15:49:40 +0800

    测试

--- app/src/main/java/com/reliance/waveloadingview/Utils.java
+++ app/src/main/java/com/reliance/waveloadingview/Utils.java
@@ -11,5 +11,10 @@ public class Utils {
     public static int convertDpToPixel(Context context, int dp) {
         float density = context.getResources().getDisplayMetrics().density;
         return Math.round((float) dp * density);
+
     }
+
+
+
+
 }
